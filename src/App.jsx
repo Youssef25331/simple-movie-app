b0bcd44600d724fab9aae7c42a433b4808d51843
Youssef25331 <yousseft25331@gmail.com>
2024-08-17 22:42:54 +0300

    Added navbar changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,28 @@
-import "./App.css";
-import Carousel from "./Carousel";
+import "./App.scss";
+
+import { Button, Container, Nav, Navbar, NavDropdown, Row, Col } from "react-bootstrap";
 
 function App() {
   return (
     <>
       <header>
-        <div className="header-container">
-          <div className="logo-contaienr">
-            <p className="logo">
-              MovieDb
-            </p>
-          </div>
-        </div>
+        <Navbar expand="lg" bg="dark" variant='dark'>
+          <Container>
+            <Navbar.Brand href="">MovieDB</Navbar.Brand>
+            <Navbar.Toggle aria-controls="toggle-navbar" />
+            <Navbar.Collapse id="toggle-navbar">
+              <Nav>
+                <Nav.Link>
+                  Movies
+                </Nav.Link>
+              </Nav>
+              <Nav>
+                <Button type="button" variant="outline-success" size="md">Search</Button>
+              </Nav>
+            </Navbar.Collapse>
+          </Container>
+        </Navbar>
       </header>
-      <Carousel />
     </>
   );
 }
