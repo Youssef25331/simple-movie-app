6ada57589b1e8c1540101c37c3e3191f4a3af111
Youssef25331 <yousseft25331@gmail.com>
2024-08-19 23:33:12 +0300

    Added Initial Carousel

--- src/Carousel.jsx
+++ src/Carousel.jsx
@@ -1,15 +1,17 @@
 import { useEffect, useState } from "react";
 
-const url = `https://api.themoviedb.org/3/movie/550?api_key=6da06cbc4125e61174358d74051c71a9`;
+function Carousel({ url, data, theme }) {
 
-function Carousel() {
-  useEffect(() => {
-    const fetchInfo = async () => {
-      const res = await fetch(url);
-      const data = await res.json();
-      console.log(data);
-    };
-    fetchInfo();
-  });
+  return (
+    <div className="carousel-wrapper">
+      {
+        data.results.map((item) =>
+          <div className="hi">
+            {item.id}
+          </div>
+        )
+      }
+    </div>
+  )
 }
 export default Carousel;
--- src/MainContent.jsx
+++ src/MainContent.jsx
@@ -2,16 +2,24 @@ import { useEffect, useState } from "react"
 import { Image } from "react-bootstrap"
 import FetchingComponent from "./FetchComponent"
 import { Alert } from "react-bootstrap"
+import Carousel from "./Carousel"
+
 function MainContent() {
 
   const { isLoading, data, url } = FetchingComponent()
   console.log(data)
-
-  console.log(isLoading)
   return (
     <div className="d-flex overflow-auto main-wrapper">
-      {<Image src="
-https://media.istockphoto.com/id/1361039427/vector/retro-download-bar-alert-window-on-computer-monitor-with-loading-message-classic-style.jpg?s=612x612&w=0&k=20&c=f9SNye6Ar9rBrotvHTVZmSZJSF9CvzBa7l0HmR8U-_8="/>
+      {
+        isLoading ?
+          (
+            <Image src="
+https://media.istockphoto.com/id/1361039427/vector/retro-download-bar-alert-window-on-computer-monitor-with-loading-message-classic-style.jpg?s=612x612&w=0&k=20&c=f9SNye6Ar9rBrotvHTVZmSZJSF9CvzBa7l0HmR8U-_8=
+        "/>
+          )
+          : (
+            <Carousel data={data} />
+          )
       }
     </div>
   )
