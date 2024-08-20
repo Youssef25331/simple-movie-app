f7a3e070cbf7dd02bfb71253c7d2ed4863f554c8
Youssef25331 <yousseft25331@gmail.com>
2024-08-20 20:57:14 +0300

    Put fetching component in SlideCarousel

--- src/MainContent.jsx
+++ src/MainContent.jsx
@@ -1,25 +1,12 @@
 import { useEffect, useState } from "react"
-import { Image } from "react-bootstrap"
-import FetchingComponent from "./FetchComponent"
-import { Alert } from "react-bootstrap"
 import SlideCarousel from "./SlideCarousel"
 
 function MainContent() {
 
-  const { isLoading, data } = FetchingComponent()
 
-  console.log(isLoading)
   return (
     <>
-      {
-
-      isLoading?(
-      <div className="hi"></div>
-      ):(
-
-      <SlideCarousel data={data} />
-      )
-      }
+      <SlideCarousel/>
     </>
 
   )
--- src/SlideCarousel.jsx
+++ src/SlideCarousel.jsx
@@ -2,8 +2,10 @@ import { useEffect, useState } from "react";
 import Carousel from "react-multi-carousel";
 import "react-multi-carousel/lib/styles.css";
 import { Image } from "react-bootstrap";
+import FetchingComponent from "./FetchComponent";
 
-function SlideCarousel({ data, theme }) {
+function SlideCarousel({ url, theme }) {
+
   const responsive = {
     desktop: {
       breakpoint: { max: 5000, min: 992 },
@@ -11,30 +13,37 @@ function SlideCarousel({ data, theme }) {
     },
     tablet: {
       breakpoint: { max: 992, min: 768 },
-      items: 2
+      items: 5
     },
     mobile: {
       breakpoint: { max: 768, min: 0 },
-      items: 1
+      items: 3
     }
   };
-  return (
 
-    <Carousel
-      responsive={responsive}
-      infinite
-      slidesToSlide={2}
-      transitionDuration={700}
-      containerClass="carousel-container"
-      centerMode
-      dotListClass="custom-dot-list-style"
-    >
-      {
-        data.results.map((items) => (
-          <Image src={"https://image.tmdb.org/t/p/w200" + items.poster_path}></Image>
-        ))
-      }
-    </Carousel >
+  const { isLoading, data } = FetchingComponent(url)
+
+  return (
+    !isLoading ? (
+
+      <Carousel
+        responsive={responsive}
+        infinite
+        slidesToSlide={2}
+        transitionDuration={700}
+        containerClass="carousel-container"
+        centerMode
+        dotListClass="custom-dot-list-style"
+      >
+        {
+          data.results.map((items) => (
+            <Image fluid key={items.id} src={"https://image.tmdb.org/t/p/w200" + items.poster_path}></Image>
+          ))
+        }
+      </Carousel >
+    ) : (
+      <></>
+    )
   )
 }
 export default SlideCarousel;
