a0a88b32d1b203e8e3e7ba82f3c003a5cc82fe86
Youssef25331 <yousseft25331@gmail.com>
2024-08-22 17:18:11 +0300

    Fixed carousel button position

--- src/MainContent.jsx
+++ src/MainContent.jsx
@@ -8,18 +8,24 @@ function MainContent() {
   const { isLoading, data } = FetchingComponent()
 
   return (
-    <div className="p-4">
+    <div className="">
       <div className="main-image-container overlay">
 
         {
           isLoading ? (<h1>Loading</h1>) : <Image className="bg-primary bg-gradient main-image" fluid src={"https://image.tmdb.org/t/p/original" + data.results[Math.floor(Math.random() * data.results.length)].backdrop_path} />
         }
       </div>
-      <div className="p-4 carocaro">
+      <div className="p-4 position-relative carousels-container">
         <SlideCarousel title={"Trending Movies"} />
         <SlideCarousel url={'https://api.themoviedb.org/3/trending/tv/day?language=en-US'} title={"Trending TV"} />
+        <SlideCarousel url={'https://api.themoviedb.org/3/movie/popular?language=en-US&page=1'} title={'Popular Movies'} />
+        <SlideCarousel url={'https://api.themoviedb.org/3/tv/popular?language=en-US&page=1'} title={"Popular TV"} />
+        <SlideCarousel url={'https://api.themoviedb.org/3/tv/top_rated?language=en-US&page=1'} title={"Top Rated TV"} />
+        <SlideCarousel url={'https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1'} title={'Top Rated Movies'} />
+        <SlideCarousel url={'https://api.themoviedb.org/3/tv/airing_today?language=en-US&page=1'} title={"Airing Today"} />
+        <SlideCarousel url={'https://api.themoviedb.org/3/movie/upcoming?language=en-US&page=1'} title={"Upcoming Movies"} />
       </div>
-    </div>
+    </div >
 
   )
 }
