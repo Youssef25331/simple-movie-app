39b0842568c3a3feec557b091dbe5596000b4b41
Youssef25331 <yousseft25331@gmail.com>
2024-08-28 04:45:09 +0300

    Fixed arrow distance on mobile

--- src/CustomArrow.jsx
+++ src/CustomArrow.jsx
@@ -1,11 +1,11 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faChevronRight, faChevronLeft } from "@fortawesome/free-solid-svg-icons";
 
-export default function CustomArrow({variant , onClick}) {
+export default function CustomArrow({ variant, onClick }) {
   return (
 
     <div onClick={onClick} className={`position-absolute transition ${variant == "right" ? "end-0 custom-arrow-right" : "start-0 custom-arrow"}  `}>
-      <FontAwesomeIcon size="2x" className="position-absolute translate-middle-y top-50 end-50 fa-icon" icon={variant == "right" ? faChevronRight : faChevronLeft} />
+      <FontAwesomeIcon size="2x" className={`position-absolute translate-middle-y top-50 fa-icon ${variant == "right" ? "start-50" : "end-50"}  `} icon={variant == "right" ? faChevronRight : faChevronLeft} />
     </div>
   )
 }
