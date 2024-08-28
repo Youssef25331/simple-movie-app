03a2547f78372854395f6048c24638848e412b20
Youssef25331 <yousseft25331@gmail.com>
2024-08-28 04:29:15 +0300

    Fixed hover overflow issue

--- src/SlideCarousel.jsx
+++ src/SlideCarousel.jsx
@@ -2,22 +2,24 @@ import { useEffect, useState } from "react";
 import Carousel from "react-multi-carousel";
 import { Container, Image } from "react-bootstrap"; import FetchingComponent from "./FetchComponent";
 import CustomArrow from "./CustomArrow";
-import Card from 'react-bootstrap/Card';
 
 function SlideCarousel({ url, title, theme }) {
 
   const responsive = {
     desktop: {
       breakpoint: { max: 5000, min: 992 },
-      items: 6
+      items: 6,
+      slidesToSlide: 6
     },
     tablet: {
       breakpoint: { max: 992, min: 768 },
-      items: 5
+      items: 5,
+      slidesToSlide: 5
     },
     mobile: {
       breakpoint: { max: 768, min: 0 },
-      items: 3
+      items: 3,
+      slidesToSlide: 3
     }
   };
 
@@ -26,30 +28,29 @@ function SlideCarousel({ url, title, theme }) {
   return (
     <Container variant='dark' className="my-5" fluid>
 
-      <h2>{title}</h2>
-      {
+      <h2 >{title}</h2> {
         !isLoading ? (
           <Carousel
-             
+
             responsive={responsive}
             customLeftArrow={
               <CustomArrow />
             }
             customRightArrow={
-              <CustomArrow  variant={'right'}/>
+              <CustomArrow variant={'right'} />
             }
             infinite
-            slidesToSlide={6}
             transitionDuration={1200}
             customTransition="all 1.2s"
-            containerClass="carousel-container"
+            containerClass="carousel-container "
             centerMode
             dotListClass="custom-dot-list-style"
+            swipeable={false}
           >
             {
               data.results.map((items) => (
-                <Image className="px-1" fluid key={items.id} src={"https://image.tmdb.org/t/p/w500" + items.poster_path}></Image>
-                
+                <Image className="px-1 carousel-image" fluid key={items.id} src={"https://image.tmdb.org/t/p/w500" + items.poster_path}></Image>
+
               ))
             }
           </Carousel >
