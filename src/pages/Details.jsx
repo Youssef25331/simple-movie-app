696ec1cce4ae2e49b7c7a956920911b40602d68e
Youssef25331 <yousseft25331@gmail.com>
2024-09-03 18:58:25 +0300

    finalized detail page style

--- src/pages/Details.jsx
+++ src/pages/Details.jsx
@@ -1,46 +1,59 @@
 import "./details.scss";
 import React from "react";
-import { Image, Row } from "react-bootstrap";
+import { Image } from "react-bootstrap";
 import FetchingComponent from "../FetchComponent";
+import getCredit from "./getCredit";
 
 export default function DetailsPage() {
-  const [data, isLoading] = FetchingComponent();
-  return (
-    <div className="details-background">
+  const { isLoading, data } = FetchingComponent(
+    "https://api.themoviedb.org/3/movie/1032823?language=en-US",
+  );
+  const { creditLoading, credit } = getCredit(true, 1032823);
+
+  return isLoading ? (
+    "loading"
+  ) : (
+    <div
+      className="details-background"
+      style={{
+        backgroundImage:
+          "url(" + "https://image.tmdb.org/t/p/original/" + data.backdrop_path,
+      }}
+    >
       <div className="position-relative d-flex details-hero-container">
         <Image
           className="details-image"
-          src="https://image.tmdb.org/t/p/w500/b33nnKl1GSFbao4l3fZDDqsMx0F.jpg"
+          src={"https://image.tmdb.org/t/p/w500" + data.poster_path}
           rounded
           fluid
         />
         <div className="ms-4 details-description-container">
-          <h2 className="details-title">THE WEIRD THINGS MOVIE</h2>
+          <h2 className="details-title">{data.title}</h2>
           <div className="d-flex flex-wrap genre">
-            <span>Fantasy</span>
-            <span>Fantasy</span>
-            <span>Fantasy</span>
-            <span>Fantasy</span>
-            <span>Fantasy</span>
+            {data.genres.map((genre) => (
+              <span key={genre.id}>{genre.name}</span>
+            ))}
           </div>
-          <p className="details-description text-break">
-            A live broadcast of a late-night talk show in 1977 goes horribly A
-            live broadcast of a late-night talk show in 1977 goes horribly
-            wrong, unleashing evil into the nation's living rooms. wrong,
-            unleashing evil into the nation's living rooms.
-          </p>
+          <p className="details-description text-break">{data.overview}</p>
           <div className="d-flex flex-column cast-container">
             <h3>Cast</h3>
-            <div className="cast-member-container d-flex">
-              <div className="cast-member align-items-center me-2 d-flex flex-column">
-                <Image src="https://image.tmdb.org/t/p/w500/b33nnKl1GSFbao4l3fZDDqsMx0F.jpg" />
-                <p className="text-center">John Doe</p>
-              </div>
 
-              <div className="cast-member align-items-center d-flex flex-column">
-                <Image src="https://image.tmdb.org/t/p/w500/b33nnKl1GSFbao4l3fZDDqsMx0F.jpg" />
-                <p className="text-center">John Doe</p>
-              </div>
+            <div className="cast-member-container d-flex flex-wrap">
+              {!creditLoading
+                ? credit.cast.slice(0, 10).map((actor) => (
+                    <div
+                      key={actor.credit_id}
+                      className="cast-member align-items-center me-2 d-flex flex-column"
+                    >
+                      <Image
+                        src={
+                          "https://image.tmdb.org/t/p/w200" + actor.profile_path
+                        }
+                      />
+                      <p className="text-center cast-member-name">{actor.name}</p>
+                    </div>
+                  ))
+                : "Cast Loading"}
             </div>
           </div>
         </div>
--- src/pages/getCredit.jsx
+++ src/pages/getCredit.jsx
@@ -4,7 +4,7 @@ export default function getCredit(isMovie, id) {
   const moviesUrl = `https://api.themoviedb.org/3/movie/${id}/credits`;
   const tvUrl = `https://api.themoviedb.org/3/tv/${id}/credits`;
   const { isLoading, data } = FetchingComponent(isMovie ? moviesUrl : tvUrl);
-  if (!isLoading) {
-    return data;
-  }
+  const creditLoading = isLoading;
+  const credit = data;
+  return { creditLoading, credit };
 }
