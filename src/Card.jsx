c9bcc7eae030a46d5b1de55b6694bdf1e9a859e4
unknown <yousseft25331@gmail.com>
2024-09-25 00:51:10 +0300

    changed card.jsx from image to div to allow for background transition

--- src/Card.jsx
+++ src/Card.jsx
@@ -9,17 +9,19 @@ export default function Card({ item, title, type }) {
         className="d-flex"
         to={type == "movies" ? "/movie/" + item.id : "/tv/" + item.id}
       >
-        <Image
-          className=" card-image"
+        <div
+          className="card-image"
           key={item.id}
-          src={
-            item.poster_path
-              ? "https://image.tmdb.org/t/p/w500" + item.poster_path
-              : "https://www.colorhexa.com/35383f.png"
-          }
+          style={{
+            backgroundImage: `url(${
+              item.poster_path
+                ? "https://image.tmdb.org/t/p/w500" + item.poster_path
+                : "https://www.colorhexa.com/35383f.png"
+            })`,
+          }}
           rounded
           fluid
-        ></Image>
+        ></div>
       </Link>
       {title ? (
         <h3 className="m-2 text-center card-title">
