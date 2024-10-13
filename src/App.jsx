8a47861d00a40ebd9fc15bbe8c615edd90feac55
unknown <yousseft25331@gmail.com>
2024-10-13 11:50:53 +0300

    changed navbar to be included with routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,63 @@
 import "./App.scss";
 import HomePage from "./HomePage";
 import { useState, createContext, useEffect } from "react";
-import { createBrowserRouter, RouterProvider, Link } from "react-router-dom";
+import { createBrowserRouter, RouterProvider, Link, Outlet } from "react-router-dom";
 import MyNavbar from "./MyNavbar";
 import Details from "./pages/Details";
 import Search from "./pages/Search";
 
+export const AppTheme = createContext();
+
+function Layout() {
+  return (
+    <div>
+      <MyNavbar />
+      <Outlet />
+    </div>
+  );
+}
+
 function App() {
   const [theme, setTheme] = useState("dark");
-  const [transparent, setTransparent] = useState(false)
+  const [transparent, setTransparent] = useState(false);
+  
   const router = createBrowserRouter([
     {
       path: "/",
-      element: <HomePage />,
-      errorElement: (
-        <h1>
-          404 not found <br />
-          <Link to="/">Click here to go back</Link>
-        </h1>
-      ),
-    },
-    {
-      path: "/:category/:id",
-      element: <Details />,
-    },
-    {
-      path: "/search/:type",
-      element: <Search />,
+      element: <Layout />, 
+      children: [
+        {
+          path: "/",
+          element: <HomePage />,
+        },
+        {
+          path: "/:category/:id",
+          element: <Details />,
+        },
+        {
+          path: "/search/:type",
+          element: <Search />,
+        },
+        {
+          path: "*",
+          element: (
+            <h1>
+              404 not found <br />
+              <Link to="/">Click here to go back</Link>
+            </h1>
+          ),
+        },
+      ],
     },
   ]);
 
   return (
     <div className="app">
       <AppTheme.Provider value={[theme, setTheme, transparent, setTransparent]}>
-        <MyNavbar />
         <RouterProvider router={router} />
       </AppTheme.Provider>
     </div>
   );
 }
-export const AppTheme = createContext();
+
 export default App;
