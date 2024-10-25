4748e62dd061cf04654386d992aa9dab3cbc00ea
unknown <yousseft25331@gmail.com>
2024-10-25 11:32:26 +0300

    made changes to details page

--- src/pages/Details.jsx
+++ src/pages/Details.jsx
@@ -8,16 +8,19 @@ import { useParams } from "react-router-dom";
 
 export default function DetailsPage() {
   const params = useParams();
-  console.log(params.category)
   const { isLoading, data } = FetchingComponent(
     `https://api.themoviedb.org/3/${params.category}/${params.id}?language=en-US`,
   );
 
+  const { isLoading: videoLoading, data: video } = FetchingComponent(
+    `https://api.themoviedb.org/3/${params.category}/${params.id}/videos?language=en-US`,
+  );
+
+  console.log(data)
   const { creditLoading, credit } = getCredit(
     params.category == "movie",
     params.id,
   );
-
   return (
     <div
       className="details-background"
@@ -39,6 +42,18 @@ export default function DetailsPage() {
             <h2 className="details-title">
               {data.title || data.name || data.original_name || "Loading"}
             </h2>
+
+            <h2 className="details-secondary-title">
+              {data.original_title || data.original_name || data.title || data.name || "Loading"}
+            </h2>
+            <div className="extra-info d-flex ">
+              <p>{!isLoading ? "🏆" + data.vote_average.toFixed(1)
+                : null}
+              </p>
+              <p>{!isLoading ?
+                params.category == "movie" ? data.release_date.substring(0, 4) : data.first_air_date.substring(0, 4) : null}
+              </p>
+            </div>
             <div className="d-flex flex-wrap genre">
               {!isLoading ?
                 data.genres.map((genre) => (
@@ -46,34 +61,48 @@ export default function DetailsPage() {
 
                     <span key={genre.id}>{genre.name}</span>
                   </a>
-                )) : ""
+                )) : null
               }
             </div>
-            <p className="details-description text-break">{data.overview}</p>
+            <p className="details-description mt-4 text-break">{data.overview}</p>
           </div>
         </div>
-        <div className="d-flex flex-column  cast-container">
+        {
+          video.results[0] ?
+            <div className="trailer-container">
+              <iframe width="1080px" height="607px"
+                src={"https://www.youtube.com/embed/" + video.results[0].key + "?autoplay=1&mute=0"}>
+              </iframe>
+            </div> : null
+        }
+        <div className="d-flex flex-column  cast-container mt-4">
           {!creditLoading && credit.cast[0] ? <h3>Cast</h3> : ""}
-          <div className="cast-member-container ms-3  d-flex flex-wrap">
+          <div className="cast-member-container  d-flex flex-wrap">
             {!creditLoading
               ? credit.cast.slice(0, 15).map((actor) =>
                 actor.profile_path ? (
-                  <div
-                    key={actor.credit_id}
-                    className="cast-member align-items-center me-2 d-flex flex-column"
-                  >
-                    <a href={"https://www.imdb.com/find/?q=" + actor.name}>
+
+                  <a href={"https://www.imdb.com/find/?q=" + actor.name}>
+                    <div
+                      key={actor.credit_id}
+                      className="cast-member align-items-center m-2 d-flex"
+                    >
                       <Image
                         src={
                           "https://image.tmdb.org/t/p/w200" +
                           actor.profile_path
                         }
                       />
-                      <p className="text-center cast-member-name">
-                        {actor.name}
-                      </p>
-                    </a>
-                  </div>
+                      <div className="ps-2">
+                        <p className="cast-member-name">
+                          {actor.name}
+                        </p>
+                        <p className="cast-member-name">
+                          {actor.character}
+                        </p>
+                      </div>
+                    </div>
+                  </a>
                 ) : (
                   ""
                 ),
@@ -82,6 +111,6 @@ export default function DetailsPage() {
           </div>
         </div>
       </div>
-    </div>
+    </div >
   );
 }
